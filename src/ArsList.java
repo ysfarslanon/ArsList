1dc1286e82a80f40727511ca6913df30409aff08
Yusuf ARSLAN <arslanon@outlook.com>
2021-09-25 00:02:07 +0300

    Two constructor, size() and getCapacity()

--- src/ArsList.java
+++ src/ArsList.java
@@ -1,2 +1,25 @@
-public class ArsList {
+public class ArsList <T> {
+    private static final int CAPACITY=10;
+    private static int size;
+    private Object[] arr;
+
+
+
+    public ArsList() {
+        arr=new Object[CAPACITY];
+        size=0;
+    }
+
+    public ArsList(int capacity) {
+        arr=new Object[capacity];
+        size=0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public int getCapacity(){
+        return arr.length;
+    }
 }
