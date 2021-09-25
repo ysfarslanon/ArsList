8c3b08088b8e0ec9cfdd2d9678a9dc945aa19ae2
Yusuf ARSLAN <arslanon@outlook.com>
2021-09-25 20:37:06 +0300

    add() and get()

--- src/ArsList.java
+++ src/ArsList.java
@@ -1,9 +1,9 @@
+import java.util.Arrays;
+
 public class ArsList <T> {
     private static final int CAPACITY=10;
     private static int size;
     private Object[] arr;
-
-
 
     public ArsList() {
         arr=new Object[CAPACITY];
@@ -22,4 +22,20 @@ public class ArsList <T> {
     public int getCapacity(){
         return arr.length;
     }
+
+    public void add(T data){
+        if (size == arr.length){
+            int newSize= arr.length * 2;
+            arr= Arrays.copyOf(arr,newSize);
+        }
+        arr[size++]=data;
+    }
+
+    public  T get(int index){
+         return arr[index]==null ? null : (T)arr[index];
+    }
+
+
+
+
 }
