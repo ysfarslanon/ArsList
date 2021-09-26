cab3f476e0ec474653a5282ae187f01bf5d4aa45
Yusuf ARSLAN <arslanon@outlook.com>
2021-09-26 23:20:51 +0300

    indexOf()

--- src/ArsList.java
+++ src/ArsList.java
@@ -73,4 +73,15 @@ public class ArsList <T> {
        result += "]";
         return result;
     }
+
+    public int indexOf(T data){
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == data) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
